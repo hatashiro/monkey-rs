e99486b786fb3845e0e47d1fb629592427a6a581
Hyunje Jun <me@noraesae.net>
2017-06-10 23:19:15 +0900

    Add REPL functionality
    Currently it just echos what is input.

--- src/repl.rs
+++ src/repl.rs
@@ -1,3 +1,43 @@
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
+use std::result;
+
 pub fn main() {
-    println!("REPL");
+    println!("The Monkey programming language REPL");
+
+    let mut editor: Editor<()> = Editor::new();
+
+    loop {
+        match editor.readline("> ") {
+            Ok(input) => {
+                match handle_input(input) {
+                    Ok(output) => println!("{}", output),
+                    Err(ReplError::DoNothing) => continue,
+                    Err(ReplError::Exit) => break,
+                }
+            }
+            Err(ReadlineError::Interrupted) => continue,
+            Err(ReadlineError::Eof) => break,
+            Err(err) => println!("{:?}", err),
+        }
+    }
+    println!("bye!");
+}
+
+type Result<T> = result::Result<T, ReplError>;
+
+enum ReplError {
+    Exit,
+    DoNothing,
+}
+
+fn handle_input(input: String) -> Result<String> {
+    if input.len() == 0 {
+        return Err(ReplError::DoNothing);
+    } else if input == "exit" {
+        return Err(ReplError::Exit);
+    }
+
+    // FIXME
+    Ok(input)
 }
