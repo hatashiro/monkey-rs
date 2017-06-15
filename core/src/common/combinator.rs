ddfe90f9d5bd523e45c8b58b552f907b2767b16c
Hyunje Jun <me@noraesae.net>
2017-06-15 17:58:45 +0900

    Change combinators into trait methods from macros

--- core/src/common/combinator.rs
+++ core/src/common/combinator.rs
@@ -1,58 +1,56 @@
-trait Parser<T, E> {
-    fn next(&mut self) -> Option<T>;
+use std::cmp::Eq;
+use std::fmt::Display;
+use std::iter::FromIterator;
+
+trait Parser<T: Display + Eq, E> {
     fn preview(&self) -> Option<&T>;
+    fn consume(&mut self) -> Option<T>;
     fn current_pos(&self) -> (i32, i32);
     fn error<S: Into<String>>(&self, message: S) -> E;
-}
 
-macro_rules! next {
-    ($p:expr) => {
-        match $p.next() {
-            Some(x) => x,
-            None => return Err($p.error("unexpected eof")),
-        }
+    fn next(&mut self) -> Result<T, E> {
+        self.consume().ok_or(self.error("unexpected eof"))
     }
-}
 
-macro_rules! predicate {
-    ($p:expr, $pred:expr) => {{
-        let x = next!($p);
-        if $pred(x) {
-            x
-        } else {
-            return Err($p.error(format!("unexpected token {}", x)));
-        }
-    }}
-}
+    fn predicate<F>(&mut self, pred: F) -> Result<T, E>
+        where F: Fn(&T) -> bool
+    {
+        self.next()
+            .and_then(|x| if pred(&x) {
+                          Ok(x)
+                      } else {
+                          Err(self.error(format!("unexpected token {}", x)))
+                      })
+    }
 
-macro_rules! atom {
-    ($p:expr, $expected:expr) => {{
-        let x = next!($p);
-        if x == $expected {
-            x
-        } else {
-            return Err(
-                $p.error(format!("unexpected token {}, expected {}", x, $expected))
-            );
-        }
-    }}
-}
+    fn atom(&mut self, expected: T) -> Result<T, E> {
+        self.next()
+            .and_then(|x| if x == expected {
+                          Ok(x)
+                      } else {
+                          Err(self.error(format!("unexpected token {}, expected {}", x, expected)))
+                      })
+    }
 
-macro_rules! string {
-    ($p:expr, $iter:expr, $t:ident) => {{
-        let i = $iter;
-        for c in &i {
-            atom!($p, *c);
+    fn string<S, O>(&mut self, s: S) -> Result<O, E>
+        where S: IntoIterator<Item = T>,
+              O: FromIterator<T>
+    {
+        let mut res: Vec<T> = Vec::new();
+        for c in s {
+            match self.atom(c) {
+                Ok(x) => res.push(x),
+                Err(err) => return Err(err),
+            }
         }
-        $t::from_iter(i)
-    }}
+        Ok(O::from_iter(res))
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::collections::vec_deque::VecDeque;
-    use std::iter::FromIterator;
 
     struct TP {
         input: VecDeque<i32>,
@@ -65,7 +63,7 @@ mod tests {
     }
 
     impl Parser<i32, String> for TP {
-        fn next(&mut self) -> Option<i32> {
+        fn consume(&mut self) -> Option<i32> {
             self.input.pop_front()
         }
 
@@ -82,89 +80,82 @@ mod tests {
         }
     }
 
-    macro_rules! with_res {
-        (test $i:ident, $b:block) => {
-            #[test]
-            fn $i() {
-                let result = || {
-                    $b;
-                    Ok(())
-                };
-                result().unwrap()
-            }
-        };
-        (panic $e:expr, $i:ident, $b:block) => {
-            #[test]
-            #[should_panic(expected = $e)]
-            fn $i() {
-                let result = || {
-                    $b;
-                    Ok(())
-                };
-                result().unwrap()
-            }
-        };
+    fn err<T>(m: &str) -> Result<T, String> {
+        Err(String::from(m))
     }
 
-    with_res!(test next_success, {
+    #[test]
+    fn next_success() {
         let mut p = TP::new(&[1, 2, 3]);
-        assert_eq!(next!(p), 1);
-        assert_eq!(next!(p), 2);
-        assert_eq!(next!(p), 3);
-    });
+        assert_eq!(p.next(), Ok(1));
+        assert_eq!(p.next(), Ok(2));
+        assert_eq!(p.next(), Ok(3));
+    }
 
-    with_res!(panic "unexpected eof", next_fail_empty, {
+    #[test]
+    fn next_fail_empty() {
         let mut p = TP::new(&[]);
-        assert_eq!(next!(p), 1);
-    });
+        assert_eq!(p.next(), err("unexpected eof"));
+    }
 
-    with_res!(test predicate_success, {
+    #[test]
+    fn predicate_success() {
         let mut p = TP::new(&[2, 4, 6]);
-        assert_eq!(predicate!(p, |x| x % 2 == 0), 2);
-        assert_eq!(predicate!(p, |x| x % 2 == 0), 4);
-        assert_eq!(predicate!(p, |x| x % 2 == 0), 6);
-    });
+        assert_eq!(p.predicate(|x| x % 2 == 0), Ok(2));
+        assert_eq!(p.predicate(|x| x % 2 == 0), Ok(4));
+        assert_eq!(p.predicate(|x| x % 2 == 0), Ok(6));
+    }
 
-    with_res!(panic "unexpected eof", predicate_fail_empty, {
+    #[test]
+    fn predicate_fail_empty() {
         let mut p = TP::new(&[]);
-        assert_eq!(predicate!(p, |x| x % 2 == 0), 2);
-    });
+        assert_eq!(p.predicate(|x| x % 2 == 0), err("unexpected eof"));
+    }
 
-    with_res!(panic "unexpected token 3", predicate_fail_not_satisfy, {
+    #[test]
+    fn predicate_fail_not_satisfy() {
         let mut p = TP::new(&[3, 5, 7]);
-        assert_eq!(predicate!(p, |x| x % 2 == 0), 3);
-    });
+        assert_eq!(p.predicate(|x| x % 2 == 0), err("unexpected token 3"));
+    }
 
-    with_res!(test atom_success, {
+    #[test]
+    fn atom_success() {
         let mut p = TP::new(&[2, 4, 6]);
-        assert_eq!(atom!(p, 2), 2);
-        assert_eq!(atom!(p, 4), 4);
-        assert_eq!(atom!(p, 6), 6);
-    });
+        assert_eq!(p.atom(2), Ok(2));
+        assert_eq!(p.atom(4), Ok(4));
+        assert_eq!(p.atom(6), Ok(6));
+    }
 
-    with_res!(panic "unexpected eof", atom_fail_empty, {
+    #[test]
+    fn atom_fail_empty() {
         let mut p = TP::new(&[]);
-        assert_eq!(atom!(p, 2), 2);
-    });
+        assert_eq!(p.atom(2), err("unexpected eof"));
+    }
 
-    with_res!(panic "unexpected token 5, expected 4", atom_fail_not_expected, {
+    #[test]
+    fn atom_fail_not_expected() {
         let mut p = TP::new(&[3, 5, 7]);
-        assert_eq!(atom!(p, 3), 3);
-        assert_eq!(atom!(p, 4), 4);
-    });
+        assert_eq!(p.atom(3), Ok(3));
+        assert_eq!(p.atom(4), err("unexpected token 5, expected 4"));
+    }
 
-    with_res!(test string_success, {
+    #[test]
+    fn string_success() {
         let mut p = TP::new(&[2, 4, 6]);
-        assert_eq!(string!(p, vec![2, 4, 6], Vec), vec![2, 4, 6]);
-    });
+        assert_eq!(p.string(vec![2, 4, 6]), Ok(vec![2, 4, 6]));
+    }
 
-    with_res!(panic "unexpected eof", string_fail_empty, {
+    #[test]
+    fn string_fail_empty() {
         let mut p = TP::new(&[]);
-        assert_eq!(string!(p, vec![2, 4, 6], Vec), [2, 4, 6]);
-    });
+        assert_eq!(p.string(vec![2, 4, 6]) as Result<Vec<i32>, String>,
+                   err("unexpected eof"));
+    }
 
-    with_res!(panic "unexpected token 5, expected 4", string_fail_not_expected, {
+    #[test]
+    fn string_fail_not_expected() {
         let mut p = TP::new(&[2, 5, 6]);
-        assert_eq!(string!(p, vec![2, 4, 6], Vec), vec![2, 4, 6]);
-    });
+        assert_eq!(p.string(vec![2, 4, 6]) as Result<Vec<i32>, String>,
+                   err("unexpected token 5, expected 4"));
+    }
 }
