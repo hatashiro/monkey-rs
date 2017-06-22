ead9fdc21767bf68c1f33df959615ac8bccd9fe9
Hyunje Jun <me@noraesae.net>
2017-06-22 18:51:47 +0900

    Declare token types

--- core/src/lexer/mod.rs
+++ core/src/lexer/mod.rs
@@ -1,7 +1,8 @@
+#[macro_use]
 pub mod types;
 
 use self::types::*;
 
 pub fn tokenize<T: Iterator<Item = char>>(input: T) -> Result<Vec<Token>> {
-    Ok(input.map(|c| Token::Char(c)).collect())
+    Ok(Vec::default())
 }
--- core/src/lexer/types.rs
+++ core/src/lexer/types.rs
@@ -2,10 +2,60 @@ use std::result;
 
 pub type Result<T> = result::Result<T, LexError>;
 
-#[derive(Debug)]
-pub enum Token {
-    Char(char),
+macro_rules! token_defs {
+    ($enum_name:ident => {
+        $($name:ident,)*
+    }) => {
+        #[derive(Debug)]
+        pub enum $enum_name {
+            $(
+                $name { row: i32, col: i32, literal: String },
+            )*
+        }
+    }
 }
+
+token_defs!(Token => {
+    Illegal,
+
+    // identifier and literals
+    Ident,
+    IntLiteral,
+    BoolLiteral,
+    StringLiteral,
+
+    // statements
+    Assign,
+    If,
+    Else,
+
+    // operators
+    Plus,
+    Minus,
+    Divide,
+    Multiply,
+    Eq,
+    NotEq,
+    GreaterThan,
+    LessThan,
+    Not,
+
+    // reserved words
+    Function,
+    Let,
+    Return,
+
+    // punctuations
+    Comma,
+    Colon,
+    SemiColon,
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+});
 
 #[derive(Debug)]
 pub struct LexError {}
