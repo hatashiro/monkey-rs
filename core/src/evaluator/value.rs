f9b166a2b102af305b82bb5aa1d618cc4a29dcf5
Hyunje Jun <me@noraesae.net>
2017-07-03 10:46:46 +0900

    Implement Display for array and hash value

--- core/src/evaluator/value.rs
+++ core/src/evaluator/value.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::fmt;
+use common::util::unescape;
 use parser::ast::*;
 use evaluator::types::*;
 
@@ -29,11 +30,18 @@ impl fmt::Display for Value {
         match self {
             &Value::Int(i) => write!(f, "{}", i),
             &Value::Bool(b) => write!(f, "{}", b),
-            &Value::String(ref s) => write!(f, "{}", s),
-            &Value::Array(ref a) => unimplemented!(), // FIXME
-            &Value::Hash(ref m) => unimplemented!(), // FIXME
+            &Value::String(ref s) => write!(f, "{}", unescape(s)),
+            &Value::Array(ref a) => {
+                let mapped: Vec<String> = a.iter().map(|v| format!("{}", v)).collect();
+                write!(f, "[{}]", mapped.join(", "))
+            },
+            &Value::Hash(ref m) => {
+                let mut mapped: Vec<String> = m.iter().map(|(h, v)| format!("{}: {}", h, v)).collect();
+                mapped.sort();
+                write!(f, "{{{}}}", mapped.join(", "))
+            },
             &Value::Fn { .. } => write!(f, "[function]"),
-            &Value::BuiltInFn { ref name, .. } => write!(f, "[built-in function: {}", name),
+            &Value::BuiltInFn { ref name, .. } => write!(f, "[built-in function: {}]", name),
             &Value::Return(ref o) => o.fmt(f),
             &Value::Null => write!(f, "null"),
         }
@@ -93,3 +101,51 @@ impl fmt::Display for Hashable {
 }
 
 pub type BuiltInFn = fn(Vec<Value>) -> Result<Value>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter::FromIterator;
+
+    fn dummy(_: Vec<Value>) -> Result<Value> {
+        unreachable!()
+    }
+
+    #[test]
+    fn value_display() {
+        assert_eq!(format!("{}", Value::Int(35)), "35");
+        assert_eq!(format!("{}", Value::Bool(true)), "true");
+        assert_eq!(format!("{}", Value::String(String::from("hello\nworld"))),
+                   "\"hello\\nworld\"");
+        assert_eq!(format!("{}", Value::Array(vec![])), "[]");
+        assert_eq!(format!("{}",
+                           Value::Array(vec![Value::Int(1), Value::Int(2), Value::Int(3)])),
+                   "[1, 2, 3]");
+        assert_eq!(format!("{}", Value::Hash(HashMap::new())), "{}");
+        assert_eq!(format!("{}",
+                           Value::Hash(HashMap::from_iter(vec![(Hashable::Int(1),
+                                                                Value::String(String::from("one"))),
+                                                               (Hashable::Int(2),
+                                                                Value::String(String::from("two"))),
+                                                               (Hashable::Int(3),
+                                                                Value::String(String::from("three")))]
+                           ))),
+                   "{1: \"one\", 2: \"two\", 3: \"three\"}");
+        assert_eq!(format!("{}",
+                           Value::Fn {
+                               params: vec![],
+                               body: vec![],
+                               env: Box::new(Env::new()),
+                           }),
+                   "[function]");
+        assert_eq!(format!("{}",
+                           Value::BuiltInFn {
+                               name: String::from("hi"),
+                               num_params: 0,
+                               func: dummy,
+                           }),
+                   "[built-in function: hi]");
+        assert_eq!(format!("{}", Value::Return(Box::new(Value::Int(35)))), "35");
+        assert_eq!(format!("{}", Value::Null), "null");
+    }
+}
